09a41e235d6ec9ec21baa59b05505eeb0215d7cc
ndwei <89673137+lwehios@users.noreply.github.com>
2025-09-02 01:19:53 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,10 +6,10 @@ const UUID = process.env.UUID || '1ba5ae30-46ba-4699-a753-fe591e903445'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'setg.mydommmmccccsss.dpdns.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGZjYmFjZjNjOGY4ZDY2YjkzZTkwMmUxYWMyMmI0MWMiLCJ0IjoiMjBjNjk5NDUtY2M5ZC00ZTEwLWIzN2YtYjVkZWVmYzAxNDMzIiwicyI6Ik4yUmpObVk0TWpBdE5qUmpNeTAwTmpBNUxXSXdNbU10WlRnek1tVmhOalE1WVdabCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'jthgdf.mydommmmccccsss.dpdns.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMGZjYmFjZjNjOGY4ZDY2YjkzZTkwMmUxYWMyMmI0MWMiLCJ0IjoiNGNjMTc2MmMtODc2Ny00OGI2LTg4NTctZmM3Mzk5YjM2MTM3IiwicyI6Ik5XRm1OelEyWWpBdE1qUXpZUzAwTVRjMkxXRmtNMkl0WVRRMlpqSXdZalUyWTJZNSJ9';
 const CFIP = process.env.CFIP || 'www.visa.com.tw';
-const NAME = process.env.NAME || 'Choreo';
+const NAME = process.env.NAME || 'Choreo3423';
 
 // root route
 app.get("/", function(req, res) {
